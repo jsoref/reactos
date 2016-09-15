f5a096326c133440e32b5aefdaf7306bc5fbb4a5
Josh Soref <jsoref@users.noreply.github.com>
2016-09-15 05:04:27 +0000

    spelling: achieve

--- reactos/sdk/include/psdk/warning.h
+++ reactos/sdk/include/psdk/warning.h
@@ -2,4 +2,4 @@
 /* FIXME
    here we putting #pragma warning and disable or enable them
    I known gcc and msvc using diffent pragma and I do not known
-   the gcc pragma well to achive msvs and gcc compatible with this */
+   the gcc pragma well to achieve msvs and gcc compatible with this */
