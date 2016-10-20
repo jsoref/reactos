154df751f32b7f8033d04a10e4ce4747b8b06a4b
Josh Soref <jsoref@users.noreply.github.com>
2016-10-20 20:25:37 +0000

    spelling: proprietary

--- reactos/drivers/ksfilter/ks/filter.c
+++ reactos/drivers/ksfilter/ks/filter.c
@@ -870,7 +870,7 @@ KspHandleDataIntersection(
                DataRange->FormatSize, DataRange->SampleSize, DataRange->Alignment, DataRange->Flags, DataRange->Reserved, DataLength);
 
         /* FIXME implement KsPinDataIntersectionEx */
-        /* Call miniport's properitary handler */
+        /* Call miniport's proprietary handler */
         Status = This->Filter.Descriptor->PinDescriptors[Pin->PinId].IntersectHandler(&This->Filter,
                                                                                       Irp,
                                                                                       Pin,
