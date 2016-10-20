34cfe371d7701e7cb9c4a7b5e35fdcedbc4778e9
Josh Soref <jsoref@users.noreply.github.com>
2016-10-20 20:44:32 +0000

    spelling: tunnel

--- rostests/kmtests/ntos_fsrtl/FsRtlTunnel.c
+++ rostests/kmtests/ntos_fsrtl/FsRtlTunnel.c
@@ -50,9 +50,9 @@ void TestFsRtlInitializeTunnelCache()
 {
     SIZE_T eq;
     T = ExAllocatePool(PagedPool, sizeof(TUNNEL));
-    ok(T != NULL, "PTUNEL is NULL after allocated memory\n");
+    ok(T != NULL, "PTUNNEL is NULL after allocated memory\n");
     Tb = ExAllocatePool(PagedPool, sizeof(TUNNEL));
-    ok(Tb != NULL, "PTUNEL is NULL after allocated memory\n");
+    ok(Tb != NULL, "PTUNNEL is NULL after allocated memory\n");
 
     memset((void*)T, 0, sizeof(TUNNEL));
     memset((void*)Tb, 0, sizeof(TUNNEL));
@@ -61,7 +61,7 @@ void TestFsRtlInitializeTunnelCache()
 
     eq = RtlCompareMemory((const VOID*)T, (const VOID*)Tb,  sizeof(TUNNEL));
 
-    ok ( eq != sizeof(TUNNEL), "FsRtlInitializeTunnelCache function did not change anything in the memory at the address PTUNEL.\n"); 
+    ok ( eq != sizeof(TUNNEL), "FsRtlInitializeTunnelCache function did not change anything in the memory at the address PTUNNEL.\n"); 
 }
 
 void TestFsRtlAddToTunnelCache(ULONGLONG DirectoryKey, PUNICODE_STRING s_name, PUNICODE_STRING l_name, BOOLEAN KeyByShortName)
