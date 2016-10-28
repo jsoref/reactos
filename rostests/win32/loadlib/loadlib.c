591b0a94e5b996e48291f6516394236894d6fde9
Josh Soref <jsoref@users.noreply.github.com>
2016-10-28 20:23:04 +0000

    spelling: sequentially

--- rostests/win32/loadlib/loadlib.c
+++ rostests/win32/loadlib/loadlib.c
@@ -104,7 +104,7 @@ int usage(const char* appName)
     dprintf("\t[ansi] - perform tests using ANSI api calls\n");
     dprintf("\t    default is %s\n", TARGET);
     dprintf("\t[loop] - run test process in continuous loop\n");
-    dprintf("\t[recurse] - load libraries recursively rather than sequentually\n");
+    dprintf("\t[recurse] - load libraries recursively rather than sequentially\n");
     dprintf("\t[debug] - enable debug mode (unused)\n");
     dprintf("\t[verbose] - enable verbose output (unused)\n");
     return 0;
