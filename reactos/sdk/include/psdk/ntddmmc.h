e686d56e6102296b1598ab010c5dcd941a2fb559
Josh Soref <jsoref@users.noreply.github.com>
2016-10-30 05:06:02 +0000

    spelling: supplemental

--- reactos/sdk/include/psdk/ntddmmc.h
+++ reactos/sdk/include/psdk/ntddmmc.h
@@ -255,7 +255,7 @@ typedef struct _FEATURE_DATA_FORMATTABLE {
 typedef struct _FEATURE_DATA_DEFECT_MANAGEMENT {
   FEATURE_HEADER Header;
   UCHAR Reserved1:7;
-  UCHAR SupplimentalSpareArea:1;
+  UCHAR SupplementalSpareArea:1;
   UCHAR Reserved2[3];
 } FEATURE_DATA_DEFECT_MANAGEMENT, *PFEATURE_DATA_DEFECT_MANAGEMENT;
 
