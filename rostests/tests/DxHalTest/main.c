7d4ef7ad7b235467d92a352fa90180d0029816e3
Josh Soref <jsoref@users.noreply.github.com>
2016-10-30 03:04:42 +0000

    spelling: studying

--- rostests/tests/DxHalTest/main.c
+++ rostests/tests/DxHalTest/main.c
@@ -102,7 +102,7 @@ int WINAPI WinMain (HINSTANCE hInst, HINSTANCE hPrevInst,
 	printf("This code have been released to some close applications with my permissions, if any company\n");
     printf("want use part or whole code, you need contact the original author to ask for permissions\n");
     printf("This code are release under alot of different licenses.\n");
-    printf("All GPL and LGPL project have right use and studing this code.\n");
+    printf("All GPL and LGPL project have right use and studying this code.\n");
     printf("This code maybe need more comment to known how stuff working and maybe looking bit messy\n");
 	printf("Bestreagds Magnus Olsen magnus@greatlord.com or greatlord@reactos.org\n");
     printf("Copyright 2006 by Magnus Olsen\n\n");
